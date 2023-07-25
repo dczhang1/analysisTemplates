9c99ff78d2c61cb5429d042a2421b9d7e04213ec
Don Zhang <dc.zhang1@gmail.com>
2023-07-24 21:15:31 -0500

    update - revise

--- 5_psychometrics.R
+++ 5_psychometrics.R
@@ -2,13 +2,11 @@
 
 ### Internal Consistency
     ### Cronbach alpha
-        psych::alpha(dplyr::select(df.clean, VALIDITY_1:VALIDITY_4), check.keys = T)
-
-
+        psych::alpha(dplyr::select(df.clean, item1:item5), check.keys = T)
 
 ### EFA/PCA
     #Create data for efa
-        efa.data <-  select(xData,var1:var5)
+        efa.data <-  select(xData,item1:item5)
     # Parallel analysis
         pa.efa.1 = fa.parallel(efa.data, fm = "pa", fa = "fa")
         pa.efa.1$fa.values
